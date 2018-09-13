69c50ccac4e73c89dac5a5812761b31510d6cdbb
oLsen <scr4t@hotmail.de>
2018-09-13 22:44:44 +0200

    settings comments

--- app/config/olbot.en.dist.php
+++ app/config/olbot.en.dist.php
@@ -1,14 +1,29 @@
 <?php
 
 return new \OLBot\Settings(
+    # bot token
     'asd',
+
+    # ID of the bot master
     '123456789',
+
+    # command settings
     [
+        # fallback replies to commands (assuming standard commands are used to add some new piece of knowledge)
         'replyToNewEntry' => 'Thank you for your contribution.',
         'replyToEntryAlreadyKnown' => 'I already know this.',
+
+        # list of commands
+        # key is the command's class name
+        # 'call' specifies which string '/call' triggers the command
+        # 'settings' is an optional array to specify a command's replies or add parameters needed in a custom command class' constructor
+        #            this might also be possible as a value: \OLBot\Model\DB\Answer::where(['category' => #no])->random()->text
         'commands' => [
             'AddJoke' => [
                 'call' => 'addJoke',
+                'settings' => [
+                    'replyToNewEntry' => 'lol'
+                ]
             ],
             'AddFlattery' => [
                 'call' => 'addFlattery',
@@ -18,6 +33,11 @@ return new \OLBot\Settings(
             ],
         ],
     ],
+
+    # instant responses as a list
+    # 'regex' specifies the responses trigger
+    # 'response' is the response (duh) added to your answer
+    # 'break' decides an instant return is triggered
     [
         [
             'regex' => '#^Hakuna$#',
@@ -25,24 +45,37 @@ return new \OLBot\Settings(
             'break' => true,
         ],
     ],
+
+    # karma settings
+    # TODO: implement functions to calculate karma (string or actual function)
     [
         'function' => '',
         'step' => 0.1,
     ],
+
+    # parser settings
     [
+        # list of categories, numbers corresponding to those in the keywords db
+        # TODO: check if category exists in list. DB might know more categories than specified here
         'categories' => [
             1 => 'Math',
             2 => 'TextResponse',
             3 => 'PictureResponse',
         ],
+
+        # math settings
         'math' => [
             'decimalPoint' => '.',
             'divisionByZeroResponse' => 'Division by Zero is evil.',
         ],
+
+        # TODO: implement this crap
         'translation' => [
             'fallbackLanguage' => 'english',
             'typicalLanguageEnding' => 'ese'
         ],
+
+        # stuff to find possible subjects, like 'foo "i am a possible subject"' and 'foo: i am too'
         'quotationMarks' => '"\'',
         'subjectDelimiters' => ':',
     ]
